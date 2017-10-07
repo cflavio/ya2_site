1520da61bb90b1b07041ca93e836530329bc7212
Flavio Calva <flavio@ya2.it>
2017-10-07 16:59:45 +0200

    publishing modifications

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.ya2.it/newsite'
+SITEURL = 'http://www.ya2.it'
 RELATIVE_URLS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
